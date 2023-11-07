a06a1fb2e482bb4962e520fe7138a0771d39b2b8
Andy Lyons <lyons.andy@gmail.com>
2023-11-06 23:30:48 -0800

    Added recordings for Part 2 and 4

--- setup/install_req_packages.R
+++ setup/install_req_packages.R
@@ -1,5 +1,5 @@
 ## Required packages
-pkgs_req <- c("tidyverse", "here", "zoo", "palmerpenguins", "rmarkdown", "conflicted", "wrkshputils", "markdown")
+pkgs_req <- c("tidyverse", "here", "zoo", "palmerpenguins", "conflicted", "wrkshputils", "markdown")
 
 ## See which ones are missing
 (pkgs_missing <- pkgs_req[!(pkgs_req %in% installed.packages()[,"Package"])])
